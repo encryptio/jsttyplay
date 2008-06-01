16f3fc31563fb55611548b24f030eb8d69c5f4d0
Chris Kastorff <encryptio@gmail.com>
2008-06-01 15:44:27 -0400

    Chunk-based playback in play.pl

--- play.pl
+++ play.pl
@@ -3,6 +3,8 @@ use warnings;
 use strict;
 $| = 1;
 
+my $readsize = 5;
+
 use Term::TtyRec;
 use TermParser;
 use FileHandle;
@@ -22,14 +24,16 @@ my $buf = '';
 my $lasttime;
 sub parseFrame {
     my ($rec, $term) = @_;
-    if ( length $buf == 0 ) {
+    if ( length $buf < $readsize ) {
         my ($time, $data) = $rec->read_next;
-        return () unless defined $time;
-        $buf .= $data;
-        $lasttime = $time;
+        return () if not defined $time and not length $buf;
+        if ( defined $time ) {
+            $buf .= $data;
+            $lasttime = $time;
+        }
     }
-    $term->parse(substr $buf, 0, 1);
-    substr($buf, 0, 1) = '';
+    $term->parse(substr $buf, 0, $readsize);
+    substr($buf, 0, $readsize) = '';
     return $lasttime;
 }
 
@@ -43,6 +47,6 @@ my $rec  = openRec($ARGV[0]);
 system("clear");
 while ( defined(my $time = parseFrame($rec, $term)) ) {
     print "\033[H".$term->as_termstring."--";
-    sleep 0.25;
+    #sleep 0.01;
 }
 
