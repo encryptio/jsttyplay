7ecbdd78552391cea8debcded96dbfd7553569e7
Chris Kastorff <encryptio@gmail.com>
2008-06-04 02:25:29 -0400

    preprocess encoder improvement: much improved multi-chunk herustics

--- preprocess.pl
+++ preprocess.pl
@@ -71,46 +71,31 @@ sub deltaFrame {
                 push @diff, [$row, ['a', substr($new[$row],0,1)]];
                 next MAINROW;
             }
+
             my @off = map { substr($old[$row], $_, 1) ne substr($new[$row], $_, 1) } 0 .. length($old[$row])-1;
             my @offchunks = ();
             for my $i ( 0 .. $#off ) {
                 if ( $off[$i] ) {
-                    if ( @offchunks and $offchunks[-1][1] == $i-1 ) {
+                    if ( @offchunks and $offchunks[-1][1] >= $i-4 ) { # coalesce if there's less than 3 chars between
                         $offchunks[-1][1] = $i;
                     } else {
                         push @offchunks, [$i,$i];
                     }
                 }
             }
-            if ( @offchunks > length($old[$row])/25 ) { # TODO: check difference lengths
-                # line mode - too many pieces
-                # too many pieces, push a single chunk
-                my $st = $offchunks[0][0];
-                my $en = $offchunks[-1][1];
-                my $chunk = substr($new[$row], $st, $en-$st+1);
-                if ( substr($chunk,0,1) x length($chunk) eq $chunk ) {
-                    # one char chunk mode
-                    push @diff, [$row+0, $st+0, $en+0, ['a',substr($chunk,0,1).""]];
+
+            for my $ch ( @offchunks ) {
+                if ( $ch->[0] == $ch->[1] ) {
+                    # char mode
+                    push @diff, [$row+0, $ch->[0]+0, substr($new[$row], $ch->[0], 1).""];
                 } else {
-                    # chunk mode
-                    push @diff, [$row+0, $st+0, $en+0, $chunk.""];
-                }
-                next MAINROW;
-            } else {
-                # worthwhile to run by chunks
-                for my $ch ( @offchunks ) {
-                    if ( $ch->[0] == $ch->[1] ) {
-                        # char mode
-                        push @diff, [$row+0, $ch->[0]+0, substr($new[$row], $ch->[0], 1).""];
+                    my $chunk = substr($new[$row], $ch->[0], $ch->[1]-$ch->[0]+1);
+                    if ( substr($chunk,0,1) x length($chunk) eq $chunk ) {
+                        # one char chunk mode
+                        push @diff, [$row+0, $ch->[0]+0, $ch->[1]+0, ['a',substr($chunk,0,1).""]];
                     } else {
-                        my $chunk = substr($new[$row], $ch->[0], $ch->[1]-$ch->[0]+1);
-                        if ( substr($chunk,0,1) x length($chunk) eq $chunk ) {
-                            # one char chunk mode
-                            push @diff, [$row+0, $ch->[0]+0, $ch->[1]+0, ['a',substr($chunk,0,1).""]];
-                        } else {
-                            # chunk mode
-                            push @diff, [$row+0, $ch->[0]+0, $ch->[1]+0, $chunk.""];
-                        }
+                        # chunk mode
+                        push @diff, [$row+0, $ch->[0]+0, $ch->[1]+0, $chunk.""];
                     }
                 }
             }
