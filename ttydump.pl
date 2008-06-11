0bb4f5161b107d2c5b9cf2f151809134c35647e0
Chris Kastorff <encryptio@gmail.com>
2008-06-11 05:16:05 -0400

    ttydump.pl: escape newlines in output

--- ttydump.pl
+++ ttydump.pl
@@ -7,7 +7,7 @@ open my $lf, "<", $ARGV[0] or die "Couldn't open $ARGV[0] for reading: $!";
 while ( read $lf, my($buf), 12 ) {
     my ($time, $ftime, $len) = unpack "VVV", $buf;
     read $lf, my($contents), $len;
-    $contents =~ s/((?!\\)[^[:print:][:space:]])/"\\x{".unpack("H*", $1)."}"/egs;
+    $contents =~ s/((?!\\)[^[:print:] \n])/"\\x{".unpack("H*", $1)."}"/egs;
     $time += $ftime / 1_000_000;
     printf "%.4f: %s\n", $time, $contents;
 }
